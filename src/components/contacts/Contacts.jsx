e4d9df9f7298df7c524ffee5683fdb58cc627595
Ildar <Ildarlaishev2@gmail.com>
2025-08-27 00:11:01 +0500

    store1

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -20,6 +20,7 @@ const Contacts = () => {
           Карта (Здесь нужно вставить компонент карты)
         </div>
 
+
         <p className={styles.address}>
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{verticalAlign: 'middle', marginRight: '5px'}}>
             <path d="M12 2C8.13401 2 5 5.13401 5 9C5 14.2289 12 22 12 22C12 22 19 14.2289 19 9C19 5.13401 15.866 2 12 2ZM12 11.5C13.3807 11.5 14.5 10.3807 14.5 9C14.5 7.61929 13.3807 6.5 12 6.5C10.6193 6.5 9.5 7.61929 9.5 9C9.5 10.3807 10.6193 11.5 12 11.5Z" fill="currentColor"/>
